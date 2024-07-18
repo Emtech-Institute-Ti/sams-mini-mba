4f628192a16b661680cf28f834d6aa7cc785dddf
Alberto Hermosillo <alberto.hermosillo@emtech.digital>
2024-07-18 17:55:45 -0600

    feat: initializing project structure

---
+++ .eslintrc.cjs
@@ -0,0 +1,29 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': ['error', { endOfLine: 'lf' }],
+    'react/prop-types': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+  },
+};
