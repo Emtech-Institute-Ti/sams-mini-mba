cc257494f795eaa90a227ca91c878259fce3a644
Alberto Hermosillo <alberto.hermosillo@emtech.digital>
2024-07-18 23:36:21 -0600

    feat: add coverage and testing reports

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'jest-environment-jsdom',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__mocks__/fileMock.js',
+  },
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['json', 'lcov', 'text', 'clover'],
+};
