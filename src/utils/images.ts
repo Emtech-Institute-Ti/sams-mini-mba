b75e903bb0fbdb0fbc4666ca14bf4840766f091c
Alberto Hermosillo <alberto.hermosillo@emtech.digital>
2024-07-31 16:51:47 -0600

    feat: add folder of images and strings

---
+++ src/utils/images.ts
@@ -0,0 +1,87 @@
+import banner from '../assets/images/banner-img.png';
+import mask from '../assets/images/mask-img.png';
+import facebook from '../assets/images/facebook.png';
+import instagram from '../assets/images/instagram.png';
+import twitter from '../assets/images/twitter.png';
+import barchart from '../assets/images/bar-chart.png';
+import clock from '../assets/images/clock.png';
+import rotate from '../assets/images/rotate.png';
+import trending from '../assets/images/trending.png';
+import usercheck from '../assets/images/user-check.png';
+import benefitimg from '../assets/images/benefit-img.png';
+import backgroundimg from '../assets/images/background-banner.png';
+import playbutton from '../assets/images/play-button.png';
+import videobackground from '../assets/images/video-background.png';
+import whatsappsupport from '../assets/images/whatsapp-support.png';
+import logouclx from '../assets/images/logo-uclx.png';
+import logouclxno from '../assets/images/logo-uclx-no.png';
+import masterlogo from '../assets/images/master-logo.png';
+import growthlogo from '../assets/images/growth-logo.png';
+import modalbackground from '../assets/images/modal-background.png';
+import checkcircle from '../assets/images/check-circle.png';
+import arrowright from '../assets/images/arrow-right.png';
+import calendaricon from '../assets/images/calendar-icon.png';
+import monitoricon from '../assets/images/monitor-icon.png';
+import notesicon from '../assets/images/notes-icon.png';
+import ownerimg from '../assets/images/owner-img.png';
+import avataricon from '../assets/images/avatar-icon.png';
+import onlinepayment from '../assets/images/online-payment.png';
+import cashpayment from '../assets/images/cash-payment.png';
+import growthbanner from '../assets/images/growth-banner.png';
+import maskpng from '../assets/images/mask-png.png';
+import maskback from '../assets/images/mask-back.png';
+import loginbanner from '../assets/images/login-banner.png';
+import masklogin from '../assets/images/mask-login.png';
+import barcode from '../assets/images/barcode.png';
+import foliobanner from '../assets/images/folio-banner.png';
+import check from '../assets/images/check.png';
+import coursebanner from '../assets/images/course-banner.png';
+import menubar from '../assets/images/menu.png';
+import line from '../assets/images/line.png';
+import coursegrowth from '../assets/images/course-growth.png';
+import coursemaster from '../assets/images/course-master.png';
+
+export {
+  banner,
+  mask,
+  facebook,
+  instagram,
+  twitter,
+  barchart,
+  clock,
+  rotate,
+  trending,
+  usercheck,
+  benefitimg,
+  backgroundimg,
+  playbutton,
+  videobackground,
+  whatsappsupport,
+  logouclx,
+  logouclxno,
+  masterlogo,
+  growthlogo,
+  modalbackground,
+  checkcircle,
+  arrowright,
+  calendaricon,
+  monitoricon,
+  notesicon,
+  ownerimg,
+  avataricon,
+  onlinepayment,
+  cashpayment,
+  growthbanner,
+  maskpng,
+  maskback,
+  loginbanner,
+  masklogin,
+  barcode,
+  foliobanner,
+  check,
+  coursebanner,
+  menubar,
+  line,
+  coursegrowth,
+  coursemaster,
+};
