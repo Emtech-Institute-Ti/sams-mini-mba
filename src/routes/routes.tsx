033267c81cb1098c95b2b6623f63e0f1a94bbeb1
Alberto Hermosillo <alberto.hermosillo@emtech.digital>
2024-07-31 16:56:16 -0600

    feat: add folder of routes

---
+++ src/routes/routes.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LandingPage from '../pages/LandingPage/LandingPage';
+import LoginPage from '../pages/LoginPage/LoginPage';
+import RegistrationFormPage from '../pages/RegistrationFormPage/RegistrationFormPage';
+import GrowthCoursePage from '../pages/GrowthPage/GrowthPage';
+import MasterCoursePage from '../pages/MasterPage/MasterPage';
+import PaymentCashPage from '../pages/PaymentCashPage/PaymentCashPage';
+import FolioFormPage from '../pages/FolioFormPage/FolioFormPage';
+import CampusDashboardPage from '../pages/CampusDashboardPage/CampusDashboardPage';
+import CourseDatePage from '../pages/CourseDatePage/CourseDatePage';
+import DashboardAccount from '../components/DashboardAccount/DashboardAccount';
+import DashboardPaymentHistory from '../components/DashboardPaymentHistory/DashboardPaymentHistory';
+import DashboardPayment from '../components/DashboardPayment/DashboardPayment';
+
+const AppRoutes: React.FC = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegistrationFormPage />} />
+        <Route path="/growthcourse" element={<GrowthCoursePage />} />
+        <Route path="/mastercourse" element={<MasterCoursePage />} />
+        <Route path="/paymentcash" element={<PaymentCashPage />} />
+        <Route path="/folioform" element={<FolioFormPage />} />
+        <Route path="/campusdashboard" element={<CampusDashboardPage />}>
+          <Route path="account" element={<DashboardAccount />} />
+          <Route path="payment-history" element={<DashboardPaymentHistory />} />
+          <Route path="payment" element={<DashboardPayment />} />
+        </Route>
+        <Route path="/coursedate" element={<CourseDatePage />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default AppRoutes;
