c07c11673c84ea9a6877634ba3cdebd219b00316
Alberto Hermosillo <alberto.hermosillo@emtech.digital>
2024-07-31 17:09:38 -0600

    feat: add initial config

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
+import AppRoutes from './routes/routes';
 
 const App: React.FC = () => {
-  return (
-    <div className="flex justify-center items-center h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-600">¡Hello World!</h1>
-    </div>
-  );
+  return <AppRoutes />;
 };
 
 export default App;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,26 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Montserrat', 'sans-serif'],
+      },
+      colors: {
+        primaryBlue: '#0067A0',
+        primaryPurple: '#000054',
+        primaryViolet: '#7865EE',
+        secondaryPurple: '#3339A3',
+        scarlet: '#491B56',
+        secondaryBlue: '#7BAEEE',
+        gray: '#1A1A1A',
+        customBlack: '#000000',
+        customBlue: '#000054',
+        customWhite: '#F5F5F5',
+        customGray: '#D9D9D9',
+        thirdBlue: '#0068A2',
+        primaryGray: '#D1D1D1',
+      },
+    },
   },
   plugins: [],
 };
